f364677112433f51602de8e5961334e7ee137782
Zeba Yasmeen <zeba.yasmeen@nw18.com>
2024-10-10 17:07:16 +0530

    attempting third commit

--- CollectionInsideTableView/CollectionInsideTableView/ViewController.swift
+++ CollectionInsideTableView/CollectionInsideTableView/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     
     
-//this is updated msg by pratik.ifius
+//this is updated msg by pratik.abhishek kadam
     @IBOutlet var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
