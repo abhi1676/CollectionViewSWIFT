c710094f873d13a0b223759044b36718a2b72421
Zeba Yasmeen <zeba.yasmeen@nw18.com>
2024-10-10 16:58:34 +0530

    i added one msg

--- CollectionInsideTableView/CollectionInsideTableView/ViewController.swift
+++ CollectionInsideTableView/CollectionInsideTableView/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     
     
-
+//this is updated msg by pratik.
     @IBOutlet var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
